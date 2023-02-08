d143be6c86914d7a04d950ffd42f2b544afb1f2d
Irina632015 <irina.632015@yandex.ru>
2023-02-08 15:44:28 +0400

    Семинар 7-52

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+var arr = GenerateArray(5, 5);
+PrintArray(arr);
+System.Console.WriteLine();
+Sum(arr);
+
+void Sum(int[,] arr)
+{
+    for(int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = arr[i, j] + sum;
+        }
+        sum = sum / arr.GetLength(0);
+        System.Console.Write(sum + "; ");
+    }
+}
+
+int[,] GenerateArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    {
+        for(int i = 0; i < result.GetLength(0); i++)
+        {
+            for(int j = 0; j < result.GetLength(1); j++)
+            {
+                    result[i, j] = new Random().Next(1, 10);
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] arr)
+{
+        {
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
